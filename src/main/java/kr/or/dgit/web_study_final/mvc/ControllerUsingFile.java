3041467c0fbf9bb635d8410f23faad7c95758592
DaeguIT-MinSuKim <net94.teacher73@gmail.com>
2018-02-21 22:12:52 +0900

    18 MVC 패턴 구현 완료

---
+++ src/main/java/kr/or/dgit/web_study_final/mvc/ControllerUsingFile.java
@@ -0,0 +1,80 @@
+package kr.or.dgit.web_study_final.mvc;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.dgit.web_study_final.mvc.command.CommandHandler;
+import kr.or.dgit.web_study_final.mvc.command.NullHandler;
+
+public class ControllerUsingFile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Map<String, CommandHandler> commandHandlerMap = new HashMap<>();
+
+	public void init() throws ServletException {
+		String configFilePath = getServletContext().getRealPath(getInitParameter("configFile"));
+		Properties prop = new Properties();
+		
+		try(InputStream fis = new FileInputStream(configFilePath)){
+			prop.load(fis);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} 
+		
+		for(String command : prop.stringPropertyNames()) {
+			String handlerName = prop.getProperty(command);
+			try {
+				CommandHandler handlerInstance = (CommandHandler)Class.forName(handlerName).newInstance();
+				commandHandlerMap.put(command, handlerInstance);
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		for(Entry<String, CommandHandler>e : commandHandlerMap.entrySet()) {
+			System.out.println(e.getKey() + " : " + e.getValue());
+		}
+	}
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String command = request.getParameter("cmd");
+		CommandHandler handler = commandHandlerMap.getOrDefault(command, new NullHandler());
+		
+		String viewPage = null;
+		try {
+			viewPage = handler.process(request, response);
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+		
+		if (viewPage != null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
---
+++ src/main/java/kr/or/dgit/web_study_final/mvc/handler/HelloHandler.java
@@ -0,0 +1,16 @@
+package kr.or.dgit.web_study_final.mvc.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.dgit.web_study_final.mvc.command.CommandHandler;
+
+public class HelloHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		req.setAttribute("hello", "안녕하세요!");
+		return "/WEB-INF/view/hello.jsp";
+	}
+
+}
